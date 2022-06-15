ba1984cef47f275b09266d4b8a040a619628d216
Tara Stahler <tbstahler@gmail.com>
2022-06-15 17:26:10 -0400

    theme and navbar/moved navbar and theme logic to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
 import './App.css';
+import { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Pages/'
 import Portfolio from './Pages/portfolio';
+import Sidebar from './Components/Sidebar'
+import Navbar from './Components/Navbar'
+import whiteLogo from './Images/TerraNovus_logo_01_REDROSES.png'
+import redLogo from './Images/TerraNovus_logo_01_REDTEXT.png'
+import {
+  DarkTheme,
+  DarkTheme_hc,
+  LightTheme,
+  LightTheme_hc,
+  darkHighlight,
+  lightHighlight,
+} from "./Theme/colors";
 
 function App() {
+  const getTheme = (dark, hc) =>
+    dark ? (hc ? DarkTheme_hc : DarkTheme) : hc ? LightTheme_hc : LightTheme;
+
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [navAlpha, setNavAlpha] = useState(1)
+  const [logo, setLogo] = useState(whiteLogo)
+  const [theme, setTheme] = useState({
+    darkMode: true,
+    highContrast: false,
+    ...getTheme(true,false)
+  })
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen)
+  const toggleNavAlpha = () => setNavAlpha(navAlpha ? 0 : 1)
+  const toggleLogo = () => setLogo(logo == whiteLogo ? redLogo : whiteLogo) 
+  const darkMode = () => setTheme({
+    darkMode: !theme.darkMode,
+    highContrast: theme.highContrast,
+    ...getTheme(!theme.darkMode, theme.highContrast)
+  })
+  const toggleHighContrast = () => setTheme({
+    darkMode: theme.darkMode,
+    highContrast: !theme.highContrast,
+    ...getTheme(theme.darkMode, !theme.highContrast)
+  })
+
+  document.getElementsByTagName('html')[0].style.background = theme.siteBg
+
   return (
     <Router className="App">
+      <Sidebar isOpen={sidebarOpen}
+        toggle={toggleSidebar}
+        siteBg={theme.siteBg}
+        siteText={theme.siteText}
+        elementBg={theme.elementBg}
+        elementText={theme.elementText}
+        highlightBg={darkHighlight}
+        highlightText={lightHighlight}
+        hoverHighlightBg={lightHighlight}
+        hoverHighlightText={darkHighlight}
+      />
+      <Navbar 
+        toggle={toggleSidebar} 
+        logoImg={logo} 
+        siteBg={theme.siteBg} 
+        siteText={theme.siteText}
+        highlightBg={darkHighlight}
+        highlightText={lightHighlight}
+        hoverHighlightBg={lightHighlight}
+        hoverHighlightText={darkHighlight}
+        opacity={navAlpha}
+      />
       <Routes>
-        <Route path='/' element={<Home />} exact />
+        <Route path='/' element={<Home theme={theme} getTheme={getTheme}/>} exact />
         <Route path='/portfolio' element={<Portfolio />} exact />
       </Routes>
     </Router>
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,81 +1,19 @@
 import React, {useState} from 'react'
-import Sidebar from '../Components/Sidebar'
-import Navbar from '../Components/Navbar'
 import HeroSection from '../Components/HeroSection'
-import whiteLogo from '../Images/TerraNovus_logo_01_REDROSES.png'
-import redLogo from '../Images/TerraNovus_logo_01_REDTEXT.png'
 import {content as infoContent} from '../Components/InfoSection/content.js'
 import {
-  DarkTheme,
-  DarkTheme_hc,
-  LightTheme,
-  LightTheme_hc,
   darkHighlight,
   lightHighlight,
 } from "../Theme/colors";
 import InfoSection from '../Components/InfoSection'
 import Services from '../Components/ServicesSection'
 
-const Home = () => {
-  const getTheme = (dark, hc) =>
-    dark ? (hc ? DarkTheme_hc : DarkTheme) : hc ? LightTheme_hc : LightTheme;
-
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-  const [navAlpha, setNavAlpha] = useState(1)
+const Home = ({theme, getTheme}) => {
   const [heroAlpha, setHeroAlpha] = useState(0)
-  const [logo, setLogo] = useState(whiteLogo)
-  const [theme, setTheme] = useState({
-    darkMode: true,
-    highContrast: false,
-    ...getTheme(true, false)
-  })
-
-  const toggleSideBar = () => {setSidebarOpen(!sidebarOpen)}
-  const toggleNavAlpha = () => {setNavAlpha(navAlpha ? 0 : 1)}
-  const toggleHeroAlpha = () => {setHeroAlpha(heroAlpha ? 0 : 0.5)}
-  const toggleLogo = () => {setLogo(logo == whiteLogo ? redLogo : whiteLogo)}
-  const toggleDarkMode = () => {
-    setTheme({
-      darkMode: !theme.darkMode,
-      highContrast: theme.highContrast,
-      ...getTheme(!theme.darkMode, theme.highContrast),
-    });
-  };
-  const toggleHighContrast = () => {
-    setTheme({
-      darkMode: theme.darkMode,
-      highContrast: !theme.highContrast,
-      ...getTheme(theme.darkMode, !theme.highContrast),
-    });
-  };
-
-  // document.getElementsByTagName('html')[0].style.background = theme.siteBg
+  const toggleHeroAlpha = () => setHeroAlpha(heroAlpha ? 0 : 0.5)
 
   return (
     <>
-      <Sidebar
-        isOpen={sidebarOpen}
-        toggle={toggleSideBar}
-        siteBg={theme.siteBg}
-        siteText={theme.siteText}
-        elementBg={theme.elementBg}
-        elementText={theme.elementText}
-        highlightBg={darkHighlight}
-        highlightText={lightHighlight}
-        hoverHighlightBg={lightHighlight}
-        hoverHighlightText={darkHighlight}
-      />
-      <Navbar 
-        toggle={toggleSideBar} 
-        logoImg={logo} 
-        siteBg={theme.siteBg} 
-        siteText={theme.siteText}
-        highlightBg={darkHighlight}
-        highlightText={lightHighlight}
-        hoverHighlightBg={lightHighlight}
-        hoverHighlightText={darkHighlight}
-        opacity={navAlpha}
-      />
       <HeroSection 
         siteBg={theme.siteBg}
         siteText={theme.siteText}
